76b2c12497facecab91e84b3ebbeb0d728d86309
rcorkrean <90876992+rcorkrean@users.noreply.github.com>
2022-11-29 16:55:45 -0500

    Create MenuBar.js

---
+++ code/API/client/src/components/MenuBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+  } from "shards-react";
+
+class MenuBar extends React.Component {
+    render() {
+        return(
+            <Navbar type="dark" theme="primary" expand="md">
+        <NavbarBrand href="/">BSGraphs</NavbarBrand>
+          <Nav navbar>
+          <NavItem>
+              <NavLink active href="/">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active href="/player_batting_bs">
+                Leaders
+              </NavLink>
+            </NavItem>
+          </Nav>
+      </Navbar>
+        )
+    }
+}
+
+export default MenuBar
