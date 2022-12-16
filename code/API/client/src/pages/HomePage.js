b964f1fee4be7e3310c521775dce2007d82b6c64
MinSeok Kim <minskim0327@gmail.com>
2022-12-15 20:23:01 -0500

    Add more descriptions regarding player batting

--- code/API/client/src/pages/HomePage.js
+++ code/API/client/src/pages/HomePage.js
@@ -19,7 +19,7 @@ class HomePage extends React.Component {
         <MenuBar />
         <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
           <h3>Welcome to BSGraphs, your premier source for statistical BS!</h3>
-          <h3>Our platform aggregates and expands upon various advanced baseball metrics provided by Baseball Savant and FanGraphs to provide users with a comprehensive search tool for baseball inquiry and analysis. </h3>
+          <p>Our platform aggregates and expands upon various advanced baseball metrics provided by Baseball Savant and FanGraphs to provide users with a comprehensive search tool for baseball inquiry and analysis. </p>
         </div>
         <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
           <h3>Facets and Features</h3>
@@ -39,6 +39,46 @@ class HomePage extends React.Component {
         </div>
         <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
           <h3>Baseball Terminologies</h3>
+          <h6>BBE</h6>
+          <p>
+            BBE(Batted Ball Event) represents any batted ball that produces a result, including outs, hits, and errors.
+          </p>
+          <h6>BB%</h6>
+          <p>
+            BB% represents the percentage of walks that occurs (when a pitcher throws four balls out of the strike zone).
+          </p>
+          <h6>K%</h6>
+          <p>
+            K% represents the strikeout rate.
+          </p>
+          <h6>EV & maxEV</h6>
+          <p>
+            EV represents the speed of the baseball as it comes off the bat, immediately after a batter makes contact. maxEV is the maximum value recorded.
+          </p>
+          <h6>Barrels/PA & BA & xBA</h6>
+          <p>
+            BA (barrels) is a ball that is hit with an EV of at least 98 mph. Barrels/PA is the number of barrels divided by the number of plate appearances. xBA is the expected value of BA.
+          </p>
+          <h6>BABIP & xBABIP</h6>
+          <p>
+            BABIP is the batting average on balls in play, which measures a player's battinng average exculsively on balls hit into the field of play, removing outcomes not affected by the opposing defense(namely home runs and strikeouts). xBABIP is the expected value of BABIP.
+          </p>
+          <h6>OBP</h6>
+          <p>
+            OBP(On Base Percentage) refers to how frequently a batter reaches base per plate apperance.
+          </p>
+          <h6>SLG</h6>
+          <p>
+            SLG(Slugging Percentage) is the number of total bases dived by the number of bats.
+          </p>
+          <h6>ISO</h6>
+          <p>
+            ISO measures the raw power of hitter by taking only extra-base hits, and the type of extra base hit into accounnt. For example a player who goes 1/6 with a double has an ISO of 0.200.
+          </p>
+          <h6>wRC+</h6>
+          <p>
+            wRC+ is a statistic that measures the runs created by a player, taking into account different league factors.
+          </p>
           <h6>WAR</h6>
           <p>
             WAR(Wins Above Replacement) is a statistic that summarize a player's total contributions to their team.
